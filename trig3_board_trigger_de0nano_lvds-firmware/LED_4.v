6d2644e31261183369010889c9d8229f48989116
drandyhaas <drandyhaas@gmail.com>
2021-07-07 13:59:55 -0400

    code for lvds board, using out10 as lvds test output (needs 3 ext resistors) go to 2.5 IO for everything, so we can use lvds output

--- trig3_board_trigger_de0nano_lvds-firmware/LED_4.v
+++ trig3_board_trigger_de0nano_lvds-firmware/LED_4.v
@@ -22,25 +22,35 @@ reg[31:0] autocounter=0; // for a rolling trigger
 reg [7:0] histostosend2;//to pass timing, since it's sent from the slow clk
 reg [7:0] calibticks2;//to pass timing, since it's sent from the slow clk
 reg [31:0] prescale2;//to pass timing, since it's sent from the slow clk
+
 always@(posedge clk_adc) begin
+	
 	pass_prescale <= (randnum<=prescale2);
 	histostosend2<=histostosend;
 	calibticks2<=calibticks;
 	prescale2<=prescale;
+	
 	i=0; while (i<16) begin
-		if (clk_locked) coaxinreg[i]<=coax_in[i];
-		else coaxinreg[i]<=0;
-		if (i<4) coax_out[i] <= (Tin[i][0]>0); // fire the channel i if board 0 has a trigger that was active on channel i
+		
+		//if (clk_locked) 
+		   coaxinreg[i]<=coax_in[i];
+		//else coaxinreg[i]<=0;
+		
+		if (i<8) coax_out[i] <= (Tin[i]>0); // fire the channel i if inputs have a trigger that was active on channel i
+		else if (i==10) coax_out[i] <= ext_trig_out; // a test lvds output trigger 
 		else coax_out[i] <= coaxinreg[i]; // passthrough
+		
 		if (i<8) histosout[i]<=histos[i][histostosend2];
+		
 		//debuging
 		//histosout[0]<=pass_prescale;
 		//histosout[1]<=randnum;
 		//histosout[2]<=prescale;
 		//histosout[2]<=triedtofire;
+		
 		i=i+1;
 	end
-	if (triedtofire==0 && (Tin[1][0]>0 && Tin[1][6]>0)) begin // fire the ext_trig output if boards 0,6 has a trigger that was active on channel 1
+	if (triedtofire==0 && (Tin[0]>0 && Tin[1]>0)) begin // fire the ext_trig output if boards 0,6 has a trigger that was active on channel 1
 		if (pass_prescale) begin
 			ext_trig_out_counter <= 4;//clk ticks to fire ext_trig for
 			autocounter <= 0;
@@ -64,74 +74,28 @@ always@(posedge clk_adc) begin
 	ext_trig_out <= (ext_trig_out_counter>0);
 end
 
-reg[31:0] spareleftcounter=0;
-always@(posedge clk_adc) begin
-	if (spareleftcounter<655) spareleft<=1; // time waiting for sync pulses, including time waiting for normal triggers to cease, 250+200+205 worst case
-	else spareleft<=0;
-	if (spareleftcounter[17+calibticks2]) spareleftcounter<=0;
-	else spareleftcounter<=spareleftcounter+1;
-end
-
-// triggers (from other boards) are synced in
-reg[1:0] Pulsecounter=0;
-reg[5:0] Trecovery[3:0][16];
-reg[3:0] Tin[4][16];
-reg[1:0] thebin[16];
+// triggers (from other boards) are read in
+reg[3:0] Tin[16];
 always @(posedge clk_adc) begin
-	if (spareleft) begin
-		if (spareleftcounter>200) begin // time to wait for normal triggers to cease
-			i=0; while (i<4) begin
-				j=0; while (j<16) begin
-					if (coaxinreg[j] && Pulsecounter==i) Trecovery[i][j] <= Trecovery[i][j]+1;
-					if (Trecovery[i][j]/2==27 && Trecovery[(i+1)%4][j]==0 && Trecovery[(i+2)%4][j]==0 && Trecovery[(i+3)%4][j]==0) delaycounter[j] <= i+1;
-					histos[i][j] <= Trecovery[i][j];
-					j=j+1;
-				end
-				i=i+1;
-			end
-		end
-		else begin
-			j=0; while (j<16) begin
-				delaycounter[j] <= 0;
-				j=j+1;
-			end
-		end
-	end
-	else begin
-		i=0; while (i<4) begin
-			j=0; while (j<16) begin
-				Trecovery[i][j] <= 0;
-				j=j+1;
-			end
-			i=i+1;
-		end
 		
 		j=0; while (j<16) begin
-			thebin[j] <= (Pulsecounter-delaycounter[j]+2)%4; // funky math - should be +1, but need an extra bin because this is not actually updated until after it is used below (gotta love FPGAs!)
 			if (coaxinreg[j]) begin
-				if (delaycounter[j]>0) begin // we have a lock
-					Tin[thebin[j]][j] <= 3; // set Tin high for this channel for 4 times this many clk ticks
-					histos[4+thebin[j]][j] <= histos[4+thebin[j]][j]+1; // record the trigger for monitoring
-				end
-				else begin
-					//we have a trigger but no lock on the triggers from the board!
-				end
+					Tin[j] <= 3; // set Tin high for this channel for this many clk ticks
+					if (!resethist) histos[4][j] <= histos[4][j]+1; // record the trigger for monitoring
 			end
-			else begin // every 4 ticks
+			else begin
 				// count down how long the triggers have been active
-				if (Tin[thebin[j]][j]>0) Tin[thebin[j]][j] <= Tin[thebin[j]][j]-1;
+				if (Tin[j]>0) Tin[j] <= Tin[j]-1;
 			end
 			if (resethist) begin
-				i=0; while (i<4) begin
-					histos[4+i][j] <= 0;
+				i=0; while (i<8) begin
+					histos[i][j] <= 0;
 					i=i+1;
 				end
 			end
 			j=j+1;
 		end
-	
-	end
-	Pulsecounter <= Pulsecounter+1; // for iterating through the trigger bins
+		
 end
 
 
